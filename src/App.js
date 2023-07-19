946512da5aff24f3a9410ed6988a704cbde93c66
Kashyap Chaganti <Kashyapchaganti96@gmail.com>
2023-07-19 05:30:42 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
+import io from "socket.io-client";
+const socket = io.connect("http://localhost:3001")
 
 function App() {
+  const [msg,setMsg]= useState("")
+  const [rmsg, setRmsg]=useState("")
+  const [room, setRoom]= useState("")
+  const roomSelect=()=>{
+    socket.emit("room",room);
+  }
+  const sendMsg = ()=>{
+    socket.emit("send_msg", {msg,room})
+    setMsg("")
+    
+  }
+  
+  useEffect(()=>{
+    socket.on("receive_msg",(x)=>{
+      setRmsg(x)
+    })
+  },[socket])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input placeholder='...enter room no' type="text" value={room} onChange={(e)=>setRoom(e.target.value)}/>
+      <button onClick ={roomSelect}>Select the Room </button>
+      <div>
+      <input placeholder='...message' type="text" value={msg} onChange={(e)=>setMsg(e.target.value)}/>
+      <button onClick ={sendMsg}>Send a Msg</button>
+      </div>
+      
+      
+      <h1>{rmsg}</h1>
     </div>
   );
 }
